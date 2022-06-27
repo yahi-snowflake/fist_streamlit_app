338fb0f274ea31ff21ef666bcce0e3a73934c990
Melimelo Melnik <108265393+yahi-snowflake@users.noreply.github.com>
2022-06-27 11:26:21 +0200

    Update streamlit_app.py
    Edited dataframe behavior: show only selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.title('My parents New Healthy Diner')
 
@@ -21,6 +21,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 
