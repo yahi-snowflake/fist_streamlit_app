1ec6a37f567f000f58092c38a3213ab45c48fa78
Melimelo Melnik <108265393+yahi-snowflake@users.noreply.github.com>
2022-06-27 16:11:34 +0200

    Update streamlit_app.py
    Added prompt for Fruit advice

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
