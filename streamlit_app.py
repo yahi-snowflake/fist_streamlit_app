2ceeccdf514a82dd924ce642c855f3326329e91d
Melimelo Melnik <108265393+yahi-snowflake@users.noreply.github.com>
2022-06-27 11:16:45 +0200

    Update streamlit_app.py
    Edited menu title
    Edited menu content

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-boiled Free-range Egg')
+streamlit.header('Breakfast favorites')
+   
+
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-boiled Free-range Egg')
+streamlit.text('🥑🍞 Avocado toast')
