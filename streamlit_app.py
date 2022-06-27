99055fe3ea524ef1f5c4b09e1bb105bcd06656b9
Melimelo Melnik <108265393+yahi-snowflake@users.noreply.github.com>
2022-06-27 11:20:03 +0200

    Update streamlit_app.py
    Added pandas
    Added csv import from S3
    Added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 
 streamlit.title('My parents New Healthy Diner')
 
@@ -12,3 +16,6 @@ streamlit.text('🐔 Hard-boiled Free-range Egg')
 streamlit.text('🥑🍞 Avocado toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
+
+
