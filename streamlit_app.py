9813cc5256a551d250da250ad47a076d01d37806
Melimelo Melnik <108265393+yahi-snowflake@users.noreply.github.com>
2022-06-27 11:14:49 +0200

    Update streamlit_app.py
    Edited typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast menu')
 streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket smoothie')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-boiled Free-range Egg')
